01dbc4e9badac46069958af2110ff0db9c008469
watashiwahuraira-tech <watashiwahuraira@gmail.com>
2025-11-23 00:13:17 +0600

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -57,7 +57,7 @@ module.exports = {
       const totalCommands = commands.size;
       msg += `\n\n⭔Bot has ${totalCommands} commands\n⭔Type ${prefix}𝐡𝐞𝐥𝐩 <𝚌𝚘𝚖𝚖𝚊𝚗𝚍 𝚗𝚊𝚖𝚎> to learn Usage.\n`;
       msg += ``;
-      msg += `\n╭─✦ADMIN: Watashi Sajib彡\n├‣ FACEBOOK\n╰‣:https://www.facebook.com/ewrsajib77?mibextid=ZbWKwL`; // customize this section if needed
+      msg += `\n╭─✦ADMIN: 𝗪𝗮𝘁𝗮𝘀𝗵𝗶 𝗪𝗮 𝗦𝗮𝗷𝗶𝗯彡\n├‣ FACEBOOK\n╰‣:https://www.facebook.com/ewrsajib77?mibextid=ZbWKwL`; // customize this section if needed
 
       try {
         const hh = await message.reply({ body: msg });
@@ -87,7 +87,7 @@ module.exports = {
         const guideBody = configCommand.guide?.en || "No guide available.";
         const usage = guideBody.replace(/{he}/g, prefix).replace(/{lp}/g, configCommand.name);
 
-        const response = `╭─────────⭓\n│ 🎀 NAME: ${configCommand.name}\n│ 📃 Aliases: ${configCommand.aliases ? configCommand.aliases.join(", ") : "Do not have"}\n├──‣ INFO\n│ 📝 𝗗𝗲𝘀𝗰𝗿𝗶𝗽𝘁𝗶𝗼𝗻: ${longDescription}\n│ 👑 𝗔𝗱𝗺𝗶𝗻: 𝐌𝐚𝐡𝐌𝐔𝐃\n│ 📚 𝗚𝘂𝗶𝗱𝗲: ${usage}\n├──‣ Usage\n│ ⭐ 𝗩𝗲𝗿𝘀𝗶𝗼𝗻: ${configCommand.version || "1.0"}\n│ ♻️ 𝗥𝗼𝗹𝗲: ${roleText}\n╰────────────⭓`;
+        const response = `╭─────────⭓\n│ 🎀 NAME: ${configCommand.name}\n│ 📃 Aliases: ${configCommand.aliases ? configCommand.aliases.join(", ") : "Do not have"}\n├──‣ INFO\n│ 📝 𝗗𝗲𝘀𝗰𝗿𝗶𝗽𝘁𝗶𝗼𝗻: ${longDescription}\n│ 👑 𝗔𝗱𝗺𝗶𝗻: 𝗪𝗮𝘁𝗮𝘀𝗵𝗶 𝗪𝗮 𝗦𝗮𝗷𝗶𝗯\n│ 📚 𝗚𝘂𝗶𝗱𝗲: ${usage}\n├──‣ Usage\n│ ⭐ 𝗩𝗲𝗿𝘀𝗶𝗼𝗻: ${configCommand.version || "1.0"}\n│ ♻️ 𝗥𝗼𝗹𝗲: ${roleText}\n╰────────────⭓`;
 
         const helpMessage = await message.reply(response);
 
